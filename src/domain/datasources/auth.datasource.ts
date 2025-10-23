ed9a03285524689c7116b30c4364ab3c12b90e7f
Jonnathan23 <jsaquicela26@gmail.com>
2025-10-23 17:18:54 -0500

    creacion de dominions

--- src/domain/datasources/auth.datasource.ts
+++ src/domain/datasources/auth.datasource.ts
@@ -2,9 +2,9 @@ import { RegisterUserDto } from "../dtos/auth/register-user.dto";
 import { UserEntity } from "../entities/user.entity";
 
 
+//todo: abstract login()
 
 export abstract class AuthDatasource {
-    //todo: abstract login()
 
     abstract register(registerUserDto: RegisterUserDto): Promise<UserEntity>;
 }
--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,5 +1,11 @@
+// datasources
+export * from './datasources/auth.datasource';
+
 // dtos
 export * from './dtos/auth/register-user.dto';
+
+// repositories
+export * from './repositories/auth.repository';
 
 // entities
 export * from './entities/user.entity';
